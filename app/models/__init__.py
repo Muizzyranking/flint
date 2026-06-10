ec36522b0efebb8138909b156a0fccee2abfdc27
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2026-06-10 09:04:09 +0100

    chore: export all models for alembic and easy access

---
+++ app/models/__init__.py
@@ -0,0 +1,6 @@
+from .job import Job
+from .job_depedencies import JobDependency
+from .job_log import JobLog
+from .settings import Setting
+
+__all__ = ["Job", "JobDependency", "JobLog", "Setting"]
