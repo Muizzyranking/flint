22605388021d0665281b85646d35d36a7547dcc7
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2026-06-12 19:03:41 +0100

    chore: pagination query params

--- app/dependencies.py
+++ app/dependencies.py
@@ -4,5 +4,7 @@ from fastapi import Depends
 from sqlalchemy.ext.asyncio.session import AsyncSession
 
 from app.db.session import get_db
+from app.schemas.pagination import PageParams
 
 DBSession = Annotated[AsyncSession, Depends(get_db)]
+PaginationParams = Annotated[PageParams, Depends(PageParams)]
